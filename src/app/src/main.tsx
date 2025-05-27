0336093435d2350e44e34e5201f7832b8ee2080e
Uroš Miletić <uros.miletic@ips-ag.com>
2025-05-27 12:59:30 +0200

    Configure Entra ID authentication and add login page (#13)
    * Configure Entra ID authentication and add login page
    
    * Update app workflow
    
    * Fix logout issue
    
    * Show email instead of principal name

--- src/app/src/main.tsx
+++ src/app/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import "./index.css";
+import App from "./App.tsx";
+import { AuthGuard } from "./auth/AuthGuard.tsx";
+import { msalConfig } from "./auth/authConfig.ts";
 
-createRoot(document.getElementById('root')!).render(
+const msalInstance = new PublicClientApplication(msalConfig);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MsalProvider instance={msalInstance}>
+      <AuthGuard>
+        <App />
+      </AuthGuard>
+    </MsalProvider>
+  </StrictMode>
+);
