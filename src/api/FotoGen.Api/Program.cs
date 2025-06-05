218379ecb4dafd4f03b04134d85be90eae7e8584
Uroš Miletić <uros.miletic@ips-ag.com>
2025-06-05 12:21:18 +0200

    Add Application Insights support (#24)
    * Add Application Insights support
    
    * Cleanup

--- src/api/FotoGen.Api/Program.cs
+++ src/api/FotoGen.Api/Program.cs
@@ -2,6 +2,7 @@ using FotoGen.Application;
 using FotoGen.Extensions.Cors;
 using FotoGen.Extensions.OpenApi;
 using FotoGen.Extensions.OpenApi.Configuration;
+using FotoGen.Extensions.OpenTelemetry;
 using FotoGen.Extensions.Security;
 using FotoGen.Infrastructure;
 using Microsoft.Extensions.Options;
@@ -14,10 +15,10 @@ builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddCustomApiVersioning();
 builder.Services.AddSwaggerGenRespectingCustomApiVersioning();
+builder.Services.ConfigureOpenTelemetry(builder.Environment);
 builder.Services.ConfigureAuthentication();
 builder.Services.ConfigureAuthorization();
 builder.Services.ConfigureCors();
-
 builder.Services.AddApplication().AddInfrastructure();
 var app = builder.Build();
 
