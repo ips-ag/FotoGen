334c6106edc4183120f3b098b9109c8df50d6cdc
Uroš Miletić <uros.miletic@ips-ag.com>
2025-06-24 12:29:44 +0200

    Add HTTP request/response logging middleware

---
+++ src/api/FotoGen.Api/Extensions/Logging/HttpLoggingExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.Options;
+
+namespace FotoGen.Extensions.Logging;
+
+public static class HttpLoggingExtensions
+{
+    public static IServiceCollection ConfigureHttpLogging(this IServiceCollection services)
+    {
+        services.AddHttpLogging();
+        services.AddSingleton<IConfigureOptions<HttpLoggingOptions>, ConfigureHttpLoggingOptions>();
+        return services;
+    }
+}
