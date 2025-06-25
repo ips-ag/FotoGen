08731a8736648552a7fc11c5ca3d812a60175623
Uroš Miletić <uros.miletic@ips-ag.com>
2025-06-25 12:05:42 +0200

    Replace invalid combinations in model name

--- src/api/FotoGen.Domain/Entities/Models/ModelName.cs
+++ src/api/FotoGen.Domain/Entities/Models/ModelName.cs
@@ -3,26 +3,35 @@ using FotoGen.Domain.Entities.Requests;
 
 namespace FotoGen.Domain.Entities.Models;
 
-public class ModelName
+public partial class ModelName
 {
     public string Value { get; }
 
     public ModelName(User user)
     {
-        Value = FormatToValidModelName($"{user.FullName}_{user.Id}");
-
+        Value = Normalize($"{user.FullName}_{user.Id}");
     }
 
     public static implicit operator string(ModelName modelName)
     {
         return modelName.Value;
     }
-    private static string FormatToValidModelName(string input)
+
+    private static string Normalize(string input)
     {
-        input = input.ToLower();
-        input = Regex.Replace(input, @"[^a-z0-9._-]", "");
-        input = Regex.Replace(input, @"^[-_.]+|[-_.]+$", "");
-        input = Regex.Replace(input, "(_-|-_)+", "");
+        input = input.Replace(" ", "_").ToLowerInvariant();
+        input = ValidCharacters().Replace(input, "");
+        input = LeadingAndTrailingSeparators().Replace(input, "");
+        input = ForbiddenCharacterCombinations().Replace(input, "");
         return input;
     }
+
+    [GeneratedRegex(@"[^a-z0-9._-]")]
+    private static partial Regex ValidCharacters();
+
+    [GeneratedRegex(@"^[-_.]+|[-_.]+$")]
+    private static partial Regex LeadingAndTrailingSeparators();
+
+    [GeneratedRegex("(_-|-_)+")]
+    private static partial Regex ForbiddenCharacterCombinations();
 }
